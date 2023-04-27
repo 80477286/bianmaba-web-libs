3ab381aefbf4d72bc82d8f5882c5b8cb35a74bf9
chenjiabin <80477286@163.com>
2023-04-27 15:45:59 +0800

    更新版本，重新发布

--- http-client/src/executors/PageableQueryExecutor.ts
+++ http-client/src/executors/PageableQueryExecutor.ts
@@ -1,11 +1,21 @@
 "use strict";
 import {AxiosInstance} from "axios";
 import QueryExecutor from "./QueryExecutor";
-import {DefaultPageableQueryResponse} from "./response/Response";
+import {DefaultPageableQueryResponse, Response} from "./response/Response";
 
 export default class PageableQueryExecutor extends QueryExecutor {
     constructor(instance: AxiosInstance, url?: string) {
         super(instance, url);
         this.setDefaultResponse(new DefaultPageableQueryResponse())
+    }
+
+    public pageSizeChange(size: number): Promise<Response> {
+        this.data.size = size;
+        return this.execute();
+    }
+
+    public pageChange(page: number): Promise<Response> {
+        this.data.page = page;
+        return this.execute();
     }
 }
