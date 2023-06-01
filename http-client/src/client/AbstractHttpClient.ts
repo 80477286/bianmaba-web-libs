6e3f3f73f72c9244fac10f29759b03a3e0908f77
chenjiabin <80477286@163.com>
2023-06-01 08:25:07 +0800

    修改：更新默认axios实例初始化参数

--- http-client/src/client/AbstractHttpClient.ts
+++ http-client/src/client/AbstractHttpClient.ts
@@ -1,12 +1,11 @@
 import {AxiosInstance, CreateAxiosDefaults, default as axios} from "axios";
-import {merge} from "@bianmaba/utils";
 
 
 export default class AbstractHttpClient {
     public defaultAxiosInstance: AxiosInstance;
 
     constructor(options: CreateAxiosDefaults | any = {}) {
-        this.defaultAxiosInstance = axios.create(merge({}, axios.defaults, options || {}))
+        this.defaultAxiosInstance = axios.create(options || {})
         this.defaultAxiosInstance.interceptors.response = axios.interceptors.response;
         this.defaultAxiosInstance.interceptors.request = axios.interceptors.request;
     }
